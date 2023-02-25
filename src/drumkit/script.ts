9cad1269a14d78426e496bb3aee2858c303ca6ce
vladpodilnyk <vlad.podilnyk@gmail.com>
2023-02-26 00:13:04 +0200

    drumkit-playground complete.

--- src/drumkit/script.ts
+++ src/drumkit/script.ts
@@ -1,3 +1,29 @@
-export function sum(left: number, right: number): number {
-    return left + right;
+const PlayingClassName = "playing";
+
+const keyPressHandler = (event: KeyboardEvent) => {
+    const code = event.key.charCodeAt(0);
+    const pad = document.querySelector(`div[data-key="${code}"]`);
+    const audio: HTMLAudioElement | null = document.querySelector(`audio[data-key="${code}"]`);
+
+    if (pad && audio) {
+        audio.currentTime = 0;
+        pad.classList.add(PlayingClassName);
+        audio.play();
+    }
 }
+
+const removeTransition = (event: Event) => {
+    const transitionEvent = event as TransitionEvent;
+    if (transitionEvent.propertyName !== "transform" || transitionEvent.target === null) {
+        return;
+    }
+    transitionEvent.target.classList.remove(PlayingClassName);
+}
+
+const handleTransitionEnd = () => {
+    const keys = document.querySelectorAll('.pad');
+    keys.forEach((value) => value.addEventListener("transitionend", removeTransition));
+}
+
+document.addEventListener("keydown", keyPressHandler);
+handleTransitionEnd();
